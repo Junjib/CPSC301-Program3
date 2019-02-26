916882b2420b91ad2b88f038036c79c6cf04a528
Junjib <junjib@cs3002b.acad.fullerton.edu>
2019-02-26 12:47:32 -0800

    Set up the various files for this program.

---
+++ pay.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include "person.h"
+#include "person.cpp"
+
+using namespace std;
+
+int main()
+{
+
+  return 0;
+}
---
+++ person.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include "person.h"
+
+using namespace std;
+
+Person::Person()
+{
+
+}
+
+void Person::setFirstName(string fName)
+{
+
+}
+
+string Person::getFirstName()
+{
+
+}
+
+void Person::setLastName(string lName)
+{
+
+}
+
+string Person::getLastName()
+{
+
+}
+
+void Person::setEmployeeId(int id)
+{
+
+}
+
+int Person::getEmployeeId()
+{
+
+}
+
+void Person::setCompanyName(string coName)
+{
+
+}
+
+string Person::getCompanyName()
+{
+
+}
+
+void Person::setPayRate(float rate)
+{
+
+}
+
+float Person::getPayRate()
+{
+
+}
+
+void Person::setHoursWorked(float hours)
+{
+
+}
+
+float Person::getHoursWorked()
+{
+
+}
+
+float Person::totalPay()
+{
+
+}
+
+string Person::fullName()
+{
+
+}
